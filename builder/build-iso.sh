1f5d7ce10051ab6f545bacb6aa29e7c3e0103303
Birk Skyum <birk.skyum@pm.me>
2026-09-06 17:23:26 +0200

    Exclude x86-only Dell amplifier package from ARM live media

--- builder/build-iso.sh
+++ builder/build-iso.sh
@@ -134,7 +134,7 @@ OMARCHY_ARCH_DROP=(
   apple-bcm-firmware apple-t2-audio-config t2fanrd linux-t2 linux-t2-headers
   macbook12-spi-driver-dkms macbook8-spi-pxa2xx-nodma-dkms
   asusctl supergfxctl rog-control-center
-  dell-xps-touchpad-haptics tuxedo-drivers-nocompatcheck-dkms
+  dell-xps-touchpad-haptics dell-xps13-sidecar-amps tuxedo-drivers-nocompatcheck-dkms
   intel-ipu7-camera intel-lpmd intel-media-driver libva-intel-driver
   thermald linux-ptl linux-ptl-headers vpl-gpu-rt libvpl
   vulkan-intel vulkan-radeon
